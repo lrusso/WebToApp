2efc77cb152e6a9cda4a5c8c8a96770bd0a64057
Leonardo Russo <info@lrusso.com>
2020-07-18 21:53:07 -0300

    Android App
    Android App

--- Android/app/src/main/java/phaser/spider/MainActivity.java
+++ Android/app/src/main/java/phaser/spider/MainActivity.java
@@ -1,9 +1,17 @@
 package phaser.spider;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 import java.io.BufferedReader;
@@ -13,6 +21,11 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends Activity
     {
+    private static ValueCallback<Uri> mUploadMessage;
+    private static ValueCallback<Uri[]> mUploadMessage5;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+    private final static int PERMISSION_REQUEST = 123;
+
     @Override protected void onCreate(Bundle savedInstanceState)
         {
         super.onCreate(savedInstanceState);
@@ -28,16 +41,150 @@ public class MainActivity extends Activity
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setAppCacheEnabled(true);
         webView.getSettings().setTextZoom(100);
+
         webView.setWebViewClient(new myWebClient());
+        webView.setWebChromeClient(new WebChromeClient()
+            {
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg)
+                {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+                }
+
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback uploadMsg, String acceptType)
+                {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+                }
+
+            //For Android 4.1
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+                {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+                }
+
+            // For Android 5.0
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> uploadMsg, FileChooserParams fileChooserParams)
+                {
+                mUploadMessage5 = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+                return true;
+                }
+            });
 
         String htmlData = loadAssetTextAsString("SpiderGame.htm", this);
         webView.loadDataWithBaseURL(null, htmlData, "text/html", "UTF-8",null);
+
+        if (Build.VERSION.SDK_INT>=23) //MARSHMALLOW
+            {
+            try
+                {
+                checkAppPermissions();
+                }
+            catch (Exception e)
+                {
+                }
+            }
+        }
+
+    @Override public void onResume()
+        {
+        super.onResume();
+        hideNavigationBar();
+        }
+
+    @Override public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+        {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        try
+            {
+            if (requestCode==PERMISSION_REQUEST)
+                {
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                    {
+                    System.exit(0);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+            }
+        }
+
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+        {
+        if (resultCode == RESULT_OK)
+            {
+            if(requestCode==FILECHOOSER_RESULTCODE)
+                {
+                if (Build.VERSION.SDK_INT>=21) //LOLLIPOP
+                    {
+                    try
+                        {
+                        Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                        mUploadMessage5.onReceiveValue(new Uri[]{result});
+                        mUploadMessage5 = null;
+                        }
+                        catch(Exception e)
+                        {
+                        }
+                    }
+                }
+                else
+                {
+                try
+                    {
+                    if (null == mUploadMessage) return;
+                    Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                    mUploadMessage.onReceiveValue(result);
+                    mUploadMessage = null;
+                    }
+                    catch(Exception e)
+                    {
+                    }
+                }
+            }
+            else
+            {
+            try
+                {
+                if (mUploadMessage5 != null)
+                    {
+                    mUploadMessage5.onReceiveValue(null);
+                    mUploadMessage5 = null;
+                    }
+
+                if (mUploadMessage != null)
+                    {
+                    mUploadMessage.onReceiveValue(null);
+                    mUploadMessage = null;
+                    }
+                }
+                catch(Exception e)
+                {
+                }
+            }
         }
 
     private String loadAssetTextAsString(String name, Context myContext)
         {
         BufferedReader in = null;
-            try
+        try
             {
             StringBuilder buf = new StringBuilder();
             InputStream is = myContext.getAssets().open(name);
@@ -78,12 +225,6 @@ public class MainActivity extends Activity
         return null;
         }
 
-    @Override public void onResume()
-        {
-        super.onResume();
-        hideNavigationBar();
-        }
-
     private void hideNavigationBar()
         {
         try
@@ -91,6 +232,23 @@ public class MainActivity extends Activity
             View decorView = getWindow().getDecorView();
             int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             decorView.setSystemUiVisibility(uiOptions);
+            }
+            catch(Exception e)
+            {
+            }
+        }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkAppPermissions()
+        {
+        try
+            {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                {
+                String[] PERMISSIONS_STORAGE = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(PERMISSIONS_STORAGE, PERMISSION_REQUEST);
+                }
             }
             catch(Exception e)
             {
