0f384d3e55d1a9328f203a6b4b3ab5ae5d841b8d
Leonardo Russo <info@lrusso.com>
2022-07-29 12:57:32 -0300

    iOS App

--- iOS/Spider/ViewController.swift
+++ iOS/Spider/ViewController.swift
@@ -11,24 +11,51 @@ import WebKit
 
 class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler
     {
-    @IBOutlet var webView: WKWebView!
+    var webView: WKWebView!
 
-    override func loadView()
-        {
-        super.loadView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // SETTING THAT THE WEBVIEW CONFIGURATION
         let webConfiguration = WKWebViewConfiguration()
         webConfiguration.userContentController.add(self, name: "webToApp")
         webConfiguration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+        webConfiguration.allowsInlineMediaPlayback = true
+        webConfiguration.mediaTypesRequiringUserActionForPlayback = []
+
+        // CREATING THE WEBVIEW WITH A BLACK BACKGROUND
+        webView = WKWebView(frame: self.view.frame, configuration: webConfiguration)
+        webView.isOpaque = true
+        webView.backgroundColor = UIColor.black
+        webView.scrollView.backgroundColor = UIColor.black
+    
+        // LOADING THE LOCAL WEB GAME URL
+        let htmlFile = Bundle.main.path(forResource: "www/SpiderGame", ofType: "htm")
+        let htmlString = try? String(contentsOfFile: htmlFile!, encoding: String.Encoding.utf8)
+        webView.loadHTMLString(htmlString!, baseURL: URL(string: "https://www.yourdomain.com"))
+
+        // ADDING THE WEBVIEW TO THE VIEW AND DELEGATING THE EVENTS
+        self.view = self.webView!
         webView.uiDelegate = self
         webView.navigationDelegate = self
+        
+        // FORCING FOCUS IN THE WEBVIEW EVERY 250 MS
+        Timer.scheduledTimer(withTimeInterval: 0.25, repeats: true) { [self] (timer) in
+            webView.becomeFirstResponder()
+            webView.setNeedsFocusUpdate()
+            webView.updateFocusIfNeeded()
         }
+    }
 
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    if let url = Bundle.main.url (forResource: "SpiderGame", withExtension: "htm", subdirectory: "www") {
-        let path = url.deletingLastPathComponent()
-        webView.loadFileURL ( url, allowingReadAccessTo: path)
-        }
+    @available(iOS 15, *)
+    func webView(
+        _ webView: WKWebView,
+        requestMediaCapturePermissionFor origin: WKSecurityOrigin,
+        initiatedByFrame frame: WKFrameInfo,
+        type: WKMediaCaptureType,
+        decisionHandler: @escaping (WKPermissionDecision) -> Void
+    ) {
+        decisionHandler(.grant)
     }
 
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
@@ -47,5 +74,4 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
         } catch {
         }
     }
-
 }
